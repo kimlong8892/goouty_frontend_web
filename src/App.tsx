ac09ffa65ce00f9f173bd7b09fef36e67a8b645c
King <king@Kings-MacBook-Air.local>
2026-01-07 18:27:47 +0700

    add_scroll_to_top

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import { PWANotificationToast } from "@/pwa/components/PWANotificationToast.tsx"
 import { PWAAlertNotification } from "@/pwa/components/PWAAlertNotification.tsx";
 import { PWANotificationProvider, usePWANotificationContext } from "@/pwa/contexts/PWANotificationContext.tsx";
 import { PWAInstallPrompt } from "@/components/PWAInstallPrompt.tsx";
+import { ScrollToTopButton } from "@/components/ScrollToTopButton.tsx";
 import Index from "./pages/Index.tsx";
 import CreateTripPage from "./pages/CreateTripPage.tsx";
 import PWACreateTripPage from "@/pwa/pages/PWACreateTripPage.tsx";
@@ -326,7 +327,12 @@ const AppContentWithRouter = ({ isPWAMode }: { isPWAMode: boolean }) => {
         <AppRoutes />
       </main>
       {/* Only show Footer if not in Mobile/PWA mode */}
-      {!isMobileView && <Footer />}
+      {!isMobileView && (
+        <>
+          <Footer />
+          <ScrollToTopButton />
+        </>
+      )}
     </div>
   );
 };
