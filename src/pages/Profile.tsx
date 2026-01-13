04db2162bc896704dd2c0d4c57dc7d5372efc3d9
King <king@Kings-MacBook-Air.local>
2026-01-13 17:59:35 +0700

    update form review

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -830,7 +830,7 @@ const Profile = () => {
                           <Star className="w-5 h-5 mr-4 text-yellow-500 group-hover:scale-110 transition-transform fill-yellow-500" /> Đánh giá trải nghiệm
                         </Button>
                       </DialogTrigger>
-                      <DialogContent className="sm:max-w-[500px] rounded-[32px]">
+                      <DialogContent className="sm:max-w-[500px] rounded-[32px] max-h-[90vh] overflow-y-auto w-full">
                         <DialogHeader>
                           <DialogTitle className="text-2xl font-bold text-center">Đánh giá & Góp ý</DialogTitle>
                         </DialogHeader>
