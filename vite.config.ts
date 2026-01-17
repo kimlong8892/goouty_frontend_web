582a9fda17cbc957d7756aceaea996660ab0a134
Long <longshare9201@gmail.com>
2026-01-17 14:46:21 +0700

    fix reload

--- vite.config.ts
+++ vite.config.ts
@@ -142,7 +142,7 @@ export default defineConfig(({ mode }) => {
           ]
         },
         devOptions: {
-          enabled: true,
+          enabled: false,
           type: 'module'
         }
       })
